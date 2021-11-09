05df7201fb685f3054f4f9f5d82cc5345f2cbd6e
HEIDER <donald.dr20@gmail.com>
2021-11-09 18:52:51 -0500

    version rama herider riveros

--- cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
+++ cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
@@ -18,7 +18,7 @@ public class Cv_ejercicio {
         // TODO code application logic here
         System.out.println("version 0");
         System.out.println("version 1");
-        System.out.println("heider riveros");
+        System.out.println("version heider riveros");
     }
     
 }
